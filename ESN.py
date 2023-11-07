6322851af01961271801e1dcf88ca847c7676d2f
dukelow9 <104810932+dukelow9@users.noreply.github.com>
2023-11-07 21:50:47 +0800

    Update ESN.py
    implemented webd

--- ESN.py
+++ ESN.py
@@ -3,6 +3,11 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow import keras
 from sklearn.model_selection import train_test_split
+import wandb
+from wandb.keras import WandbCallback
+
+wandb.login()
+wandb.init(project='esn-experiment')
 
 class EchoStateNetwork(keras.layers.Layer):
     def __init__(self, reservoirSize, spectralRadius=0.9, sparsity=0.2, seed=None, **kwargs):
@@ -15,30 +20,30 @@ class EchoStateNetwork(keras.layers.Layer):
     def build(self, inputShape):
         self.reservoirWeights = self.add_weight(
             "reservoirWeights",
-            shape = (inputShape[-1], self.reservoirSize),
-            initializer = tf.initializers.glorot_uniform(seed=self.seed),
-            trainable = False,
+            shape=(inputShape[-1], self.reservoirSize),
+            initializer=tf.initializers.glorot_uniform(seed=self.seed),
+            trainable=False,
         )
         self.recurrentWeights = self.add_weight(
             "recurrentWeights",
-            shape = (self.reservoirSize, self.reservoirSize),
-            initializer = tf.initializers.orthogonal(gain=self.spectralRadius, seed=self.seed),
-            trainable = False,
+            shape=(self.reservoirSize, self.reservoirSize),
+            initializer=tf.initializers.orthogonal(gain=self.spectralRadius, seed=self.seed),
+            trainable=False,
         )
         self.bias = self.add_weight(
             "bias",
-            shape = (self.reservoirSize,),
-            initializer = "zeros",
-            trainable = False,
+            shape=(self.reservoirSize,),
+            initializer="zeros",
+            trainable=False,
         )
         super(EchoStateNetwork, self).build(inputShape)
 
     def call(self, inputs):
-        x = tf.tanh(tf.matmul(inputs, self.reservoirWeights) + self.bias)
+        x = tf.matmul(inputs, self.reservoirWeights) + self.bias
         x = tf.tanh(tf.matmul(x, self.recurrentWeights))
         return x
 
-dataLength = 2000
+dataLength = 1000
 t = np.linspace(0, 60 * np.pi, dataLength)
 opticalData = (0.5 * np.sin(0.1 * t) + 0.2 * np.sin(0.5 * t) + 0.3 * np.sin(1.0 * t) + 0.1 * np.random.randn(dataLength))
 
@@ -55,27 +60,30 @@ Y = np.array(Y)
 xTrain, xTest, yTrain, yTest = train_test_split(X, Y, test_size=0.2, shuffle=False)
 
 reservoirSize = 400
-esn = EchoStateNetwork(reservoirSize = reservoirSize)
+esn = EchoStateNetwork(reservoirSize=reservoirSize)
 
-outputLayer = keras.layers.Dense(1, activation = "linear")
+outputLayer = keras.layers.Dense(1, activation="linear", trainable=True)
 
 inputData = keras.layers.Input(shape=(sequenceLength,))
 ESNOutput = esn(inputData)
 output = outputLayer(ESNOutput)
 
 model = keras.models.Model(inputs=inputData, outputs=output)
-model.compile(optimizer = "adam", loss = "mean_squared_error")
+model.compile(optimizer="adam", loss="mean_squared_error")
 
 epochCount = 100
 for epoch in range(epochCount):
     history = model.fit(xTrain, yTrain, epochs=1, verbose=0)
     trainLoss = history.history['loss'][0]
+    wandb.log({'epoch': epoch + 1, 'train_loss': trainLoss})
     testLoss = model.evaluate(xTest, yTest, verbose=0)
+    wandb.log({'epoch': epoch + 1, 'test_loss': testLoss})
     print(f"Epoch {epoch + 1}/{epochCount}  ---  Train Loss: {trainLoss:.4f}  ---  Test Loss: {testLoss:.4f}")
 
-predictions = model.predict(xTest)
 testLoss = model.evaluate(xTest, yTest)
+wandb.log({'test_loss': testLoss})
 print(f"Test Loss: {testLoss:.4f}")
+predictions = model.predict(xTest)
 
 plt.figure(figsize=(12, 6))
 plt.subplot(2, 1, 1)
